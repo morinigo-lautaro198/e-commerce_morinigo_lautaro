abcc95ee526688ba36d2b1c4d39e9a79a118a331
lol <lol@gmail.com>
2024-08-29 20:42:17 -0300

    imagenes modificadas js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hola");
+const h5 = document.querySelector("h5");
+const text = document.querySelector(".card-text");
+const img = document.querySelector(".card-img-top")
+h5.innerText = "Dibu";
+
+text.innerHTML= "lo mas grande";
+
+img.src="https://media.revistavanityfair.es/photos/63a0486adab86a3405e1db0a/1:1/w_2067,h_2067,c_limit/GettyImages-1450157276.jpg"
+
