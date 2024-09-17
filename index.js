1e96b0f5c6496742c30d7a7a3ba485d4125fb6a6
lol <lol@gmail.com>
2024-09-17 18:56:08 -0300

    card 3 d

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const h5 = document.querySelector("h5");
-const text = document.querySelector(".card-text");
-const img = document.querySelector(".card-img-top")
-h5.innerText = "Dibu";
-
-text.innerHTML= "lo mas grande";
-
-img.src="https://media.revistavanityfair.es/photos/63a0486adab86a3405e1db0a/1:1/w_2067,h_2067,c_limit/GettyImages-1450157276.jpg"
-
+function insertarauto(){
+  const h1 = document.querySelector("h1");
+  
+  const section = document.querySelector("section");
+  h1.innerText = "Productos";
+  
+  const a = document.querySelector("a");
+  a.innerText = "Ver mas";
+  
+  let array = []
+  
+  for (let i = 1; i < 9; i++) {
+  
+      array.push(` <div class="card" style="width: 18rem;">
+    <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card ${i}</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>`)
+  
+  section.innerHTML = array.join().replaceAll(",", "");
+  }
+  }
+  insertarauto()
+  
