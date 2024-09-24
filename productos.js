11cc27b094c52a70a029c11c69407c9ab94bdc8d
lol <lol@gmail.com>
2024-09-24 19:34:01 -0300

    Insertar el contenido de la variable en el main de producto.html con innerHTML.

--- productos.js
+++ productos.js
@@ -1,17 +1,31 @@
-class Productos{
-    constructor(titulo, detalle, precio, stock, imagen){
+class Productos {
+    constructor(titulo, detalle, precio, stock, imagen) {
         this.titulo = titulo;
         this.detalle = detalle;
-        this.precio = precio
+        this.precio = precio;
         this.stock = stock;
         this.imagen = imagen;
-
     }
 }
-    
 
-    let objeto = new Productos("auto rojo","Nuevo de fabrica",9000,20,"https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg")
-    const productohtml = '<div>titulo : ${Productos.titulo}</div>';
-    const main = document.querySelector("main");
-    main.innerHTML = productohtml();
-    console.log($Productos.titulo)
+let objeto = new Productos(
+    "auto rojo",
+    "Nuevo de fabrica",
+    9000,
+    20,
+    "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg"
+);
+
+let productohtml = `
+    <div>
+        <h2>${objeto.titulo}</h2>
+        <p>${objeto.detalle}</p>
+        <p>Precio: $${objeto.precio}</p>
+        <p>Stock: ${objeto.stock}</p>
+        <img src="${objeto.imagen}" width="300" height="200">
+    </div>
+`;
+
+const main = document.querySelector("main");
+main.innerHTML = productohtml;
+
